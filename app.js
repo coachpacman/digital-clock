a1ae6e6d4674ec06e5b15a885aed8be311195123
Tim Lee <tim.wenyang.lee@gmail.com>
2023-10-05 14:06:06 -0700

    add basic styling

--- app.js
+++ app.js
@@ -97,16 +97,3 @@ dayStr = day + ", " + month + " " + date + suffix + " " + year
 document.getElementById('date-display').innerHTML = dayStr;
 
 
-
-// Function from SO 
-// function formatAMPM(date) {
-//   var hours = date.getHours();
-//   var minutes = date.getMinutes();
-//   var seconds = date.getSeconds();
-//   var ampm = hours >= 12 ? 'PM' : 'AM';
-//   hours = hours % 12;
-//   hours = hours ? hours : 12; // the hour '0' should be '12'
-//   minutes = minutes < 10 ? '0'+minutes : minutes;
-//   var strTime = hours + ':' + minutes + ':' + seconds + " " + ampm;
-//   document.getElementById("time-display").innerHTML = strTime
-// }
