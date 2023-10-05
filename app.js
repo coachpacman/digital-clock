a0d62c1f87e8047952baecd9395bffc46e3116fc
Tim Lee <tim.wenyang.lee@gmail.com>
2023-10-05 13:41:00 -0700

    Add human readable day, date, month, year

--- app.js
+++ app.js
@@ -9,20 +9,11 @@
 
 //"How could I get the current second to show up and tick each second?"
 
-  // function secondIncrement() {
-  //   const date = new Date();
-  //   document.getElementById('time-display').innerHTML = date.getSeconds();
-  // }
+//What is the timeformat of .getHours, .getMinutes., getSeconds?
+    //Does not show leading zero for single digit numbers
 
-  // setInterval(secondIncrement, 1000);
-
-//"How do I show current hour, current minute, current second in one line?"
-// const date = new Date();
-// var hours = date.getHours();
-// var minutes = date.getMinutes();
-// var seconds = date.getSeconds();
-// var strTime = hours + ":" + minutes + ":" + seconds;
-// document.getElementById('time-display').innerHTML = strTime;
+//How do you add '0' for single digit returns?
+  //https://hardiquedasore.medium.com/simplest-way-to-add-a-leading-zero-to-a-number-in-javascript-b8724749486f
 
 //"How do I show get seconds to increment in the string?" 
     // TRY TO UPDATE "SECONDS" --> DOESN'T WORK
@@ -59,11 +50,52 @@
 
     setInterval(timeUpdate, 1000)
     
-//What is the timeformat of .getHours, .getMinutes., getSeconds?
-    //Does not show leading zero for single digit numbers
+//"How can I get my name to show up underneath the time?"
+  //document.getElementById('date-display').innerHTML = "Tim"
+//"How can I get a day to show up underneath the time?"
+    //"How do I change day number to a string?"
+var dayStr;
 
-//How do you add '0' for single digit returns?
-  //https://hardiquedasore.medium.com/simplest-way-to-add-a-leading-zero-to-a-number-in-javascript-b8724749486f
+var date = new Date();
+
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+var day = date.getDate();
+day = days[day]; //convert interger day to human readable day
+
+//"How can I get a month to show up underneath the time?"
+    //"How do I change day number to a string?"
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+var month = date.getMonth();
+month = months[month] //convert interger month to human readable month
+
+//"How can I get a date to show up underneath the time?"
+    //"How do I added suffixes to dates?"
+var date = date.getDate();
+//function to add suffixes for dates
+var suffix;
+function nthNumber(number) {
+  if (number > 3 && number < 21) return "th";
+  switch (number % 10) {
+    case 1:
+      return "st";
+    case 2:
+      return "nd";
+    case 3:
+      return "rd";
+    default:
+      return "th";
+  }
+}
+
+suffix = nthNumber(date)
+
+//"How can I get a year to show up underneath the time?"
+var year = date.getFullYear() // date.getFullYear() is not a function
+
+//"How do I string together day, month, date, year in one line?"
+dayStr = day + ", " + month + " " + date + suffix + year
+document.getElementById('date-display').innerHTML = dayStr;
+
 
 
 // Function from SO 
