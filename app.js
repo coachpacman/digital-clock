37f0c0a244f7f854c597182f5b6d6040fc2708b0
Tim Lee <tim.wenyang.lee@gmail.com>
2023-10-08 12:49:33 -0700

    call setInetrval globally, create toggleTime()

--- app.js
+++ app.js
@@ -3,26 +3,11 @@ const d = new Date();
 var date = d.getDate();
 var hours = d.getHours();
 var ampm;
-ampm = hours > 12 ? "PM" : "AM" 
+ampm = hours >= 12 ? "PM" : "AM" 
 
 function timeUpdate() { 
   var n = new Date();
-
-  if (timeIsInMilitaryTime === true) {
-    //format for non-military time
-    hours = hours % 12; //convert to non-military time
-    timeIsInMilitaryTime = false; //change flag
-    document.getElementById("toggle-button").innerHTML = "Change to 24-hour clock"
-    console.log('a')
-  } else {
-    hours = d.getHours();
-    if (hours === 0) { //account for the midnight hour
-      hours = 12;
-    }
-    timeIsInMilitaryTime = true;
-    document.getElementById("toggle-button").innerHTML = "Change to 12-hour clock"
-    console.log('b')
-  }
+  
   //add leading zero for hours
   if (hours < 10) {
     hours = hours.toString().padStart(2, '0');
@@ -39,33 +24,33 @@ function timeUpdate() {
   if (seconds < 10) {
     seconds = seconds.toString().padStart(2, '0');
   }
-  // timeToggle();
+  // renderDateAndTime();
   var strTime = hours + ":" + minutes + ":" + seconds + " " + ampm;
   document.getElementById('time-display').innerHTML = strTime;
-  setInterval(timeUpdate, 1000)
 
 }
 
-// function timeToggle() {
-//   if (timeIsInMilitaryTime === true) {
-//     //format for non-military time
-//     hours = hours % 12; //convert to non-military time
-//     timeIsInMilitaryTime = false; //change flag
-//     document.getElementById("toggle-button").innerHTML = "Change to 24-hour clock"
-//     console.log('a')
-//   } else {
-//     hours = d.getHours();
-//     if (hours === 0) { //account for the midnight hour
-//       hours = 12;
-//     }
-//     timeIsInMilitaryTime = true;
-//     document.getElementById("toggle-button").innerHTML = "Change to 12-hour clock"
-//     console.log('b')
-//   }
-// }
-
-// timeToggle(); // trying to make the default time be non-military
-timeUpdate(); // call function so it shows time on opening page
+function toggleTime() {
+  if (timeIsInMilitaryTime === true) {
+    
+    timeIsInMilitaryTime = false; //change flag
+    document.getElementById("toggle-button").innerHTML = "Change to 12-hour clock"
+    console.log('a')
+  } else {
+    hours = d.getHours();
+    //format for non-military time
+    hours = hours % 12; //convert to non-military time
+    if (hours === 0) { //account for the midnight hour
+      hours = 12;
+    }
+    timeIsInMilitaryTime = true;
+    document.getElementById("toggle-button").innerHTML = "Change to 24-hour clock"
+    console.log('b')
+  }
+}
+toggleTime();
+timeUpdate();
+setInterval(timeUpdate, 1000)
 
 
 var dayStr;
@@ -99,43 +84,3 @@ suffix = nthNumber(date)
 
 dayStr = day + ", " + month + " " + date + suffix + " " + year
 document.getElementById('date-display').innerHTML = dayStr;
-
-//Add button onto page
-//Button triggers alert function
-//Add second button onto page
-//Second button triggers different alert function
-//Function for military time
-//Buttons in one line
-//When one function is running, how do you end the other function?
-//How do you get one function to stop and then trigger the next function?
-//One function for getting time; one function for converting time to military or non-military time
-//Global timeIsInMilitaryTime; variable
-
-// function militaryTimeUpdate() {
-//   const date = new Date();
-//   var hours = date.getHours();
-//   var ampm;
-//   ampm = hours >= 12 ? "PM" : "AM" 
-
-//   //add leading zero for hours
-//   if (hours < 10) {
-//     hours = hours.toString().padStart(2, '0');
-//   }
-
-//   var minutes = date.getMinutes();      
-//   //add leading zero for minutes
-//   if (minutes < 10) {
-//     minutes = minutes.toString().padStart(2, '0');
-//   }
-
-//   var seconds = date.getSeconds();
-//   //add leading zero for seconds
-//   if (seconds < 10) {
-//     seconds = seconds.toString().padStart(2, '0');
-//   }
-
-//   var strTime = hours + ":" + minutes + ":" + seconds + " " + ampm;
-
-//   document.getElementById('time-display').innerHTML = strTime;
-//   setInterval(militaryTimeUpdate, 1000)
-// }
